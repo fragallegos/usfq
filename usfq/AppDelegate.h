0bd11c5c22511d3245aeeedae3b2778985a9abc8
Cristina Leon <stoyhot@MacBook-Pro-de-Jessica-leon.local>
2012-01-21 16:19:35 -0500

    Initial Commit

---
+++ usfq/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  usfq
+//
+//  Created by Cristina leon on 21/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ usfq/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  usfq
+//
+//  Created by Cristina leon on 21/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
